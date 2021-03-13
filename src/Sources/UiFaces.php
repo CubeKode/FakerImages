144c4fbf072c47270102c1d413a61a72fb2ea936
Samuel Moraes <samuel@cubekode.com>
2021-03-13 18:10:18 -0300

    Caching

--- src/Sources/UiFaces.php
+++ src/Sources/UiFaces.php
@@ -4,10 +4,16 @@ namespace CubeKode\Faker\Sources;
 
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Psr7\Response;
-use Illuminate\Support\Arr;
 
 abstract class UiFaces
 {
+    /**
+     * Cached requests
+     *
+     * @var array
+     */
+    protected static $cached = [];
+
     /**
      * Get HTTP client for Pixabay requests
      *
@@ -46,6 +52,42 @@ abstract class UiFaces
     }
 
     /**
+     * Get cache key identifier for a given request
+     *
+     * @param boolean $male
+     * @param integer $from
+     * @param integer $to
+     * @return string
+     */
+    protected static function getCachedKey(bool $male = null, int $from = null, int $to = null): string
+    {
+        return implode('-', [$male ? 1 : 0, $from, $to]);
+    }
+
+    /**
+     * Store cache
+     *
+     * @param string $key
+     * @param array $values
+     * @return void
+     */
+    protected static function setCache(string $key, array $values): void
+    {
+        self::$cached[$key] = $values;
+    }
+
+    /**
+     * Check if a given request is cached
+     *
+     * @param string $key
+     * @return boolean
+     */
+    protected static function isCached(string $key): bool
+    {
+        return count(self::$cached[$key] ?? []) > 0;
+    }
+
+    /**
      * Send request to the random faces endpoint
      *
      * @param  bool|null $male Male or female, or null to random result
@@ -55,15 +97,21 @@ abstract class UiFaces
      */
     public static function getFace(bool $male = null, int $from = null, int $to = null): string
     {
-        $response = self::request([
-            'limit' => 1,
-            'random' => true,
-            'gender' => $male === null ? '' : [($male ? 'male' : 'female')],
-            'provider' => [1],
-            'from_age' => $from ?? 18,
-            'to_age' => $to ?? 40,
-        ]);
+        $key = self::getCachedKey($male, $from, $to);
 
-        return Arr::get($response, '0.photo');
+        if (!self::isCached($key)) {
+            $response = self::request([
+                'limit' => 30,
+                'random' => true,
+                'gender' => $male === null ? '' : [($male ? 'male' : 'female')],
+                'provider' => [1],
+                'from_age' => $from ?? 18,
+                'to_age' => $to ?? 40,
+            ]);
+
+            self::setCache($key, $response);
+        }
+
+        return array_pop(self::$cached[$key])['photo'];
     }
 }
